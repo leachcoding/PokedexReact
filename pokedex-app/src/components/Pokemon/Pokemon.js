abdee0b20beece455b92830cd93a571c7cae9c9d
Jay Leach <leachcoding@gmail.com>
2020-03-24 10:33:27 -0400

    Starting refactors to mobile friendly, added some conditional logic for the id, changed the font

--- pokedex-app/src/components/Pokemon/Pokemon.js
+++ pokedex-app/src/components/Pokemon/Pokemon.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route, Link } from "react-router-dom";
 import './Pokemon.css';
 import StatsPage from '../StatsPage/StatsPage.js';
 
 const Pokemon = props => {
 
+  const[id, setId] = useState(props.data.id);
+
   const capitalize = (s) => {
     if (typeof s !== 'string') return ''
     return s.charAt(0).toUpperCase() + s.slice(1)
   }
+
+  let idVal = '';
+
+  if(props.data.id < 10) {
+    idVal = 'Id#: 00' + id.toString();
+  } else if(props.data.id < 100) {
+    idVal = 'Id#: 0' + id.toString();
+  } else {
+    idVal = 'Id#: ' + id.toString();
+  }
+
+  console.log(idVal);
 
   return (
     <>
     <div className="card">
       <Link to={`/detail/${props.data.id}`}>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.data.id}.png`} alt="pokemon" />
-      <p>Id#: {props.data.id}</p>
+      <p>{idVal}</p>
       <p>Name: {capitalize(props.data.name)}</p>
       </Link>
     </div>
